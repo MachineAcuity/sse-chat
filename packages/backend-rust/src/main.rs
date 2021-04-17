393de650432814e34e57d519b3630a5b56a03825
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2021-04-17 04:25:05 -0400

    Upgrade warp to 0.3

--- packages/backend-rust/src/main.rs
+++ packages/backend-rust/src/main.rs
@@ -1,30 +1,217 @@
-use log::*;
-use warp::Filter;
+use futures::{Stream, StreamExt};
+use std::collections::HashMap;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc, Mutex,
+};
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use warp::{hyper::body::Bytes, sse::Event, Filter};
 
-fn main() {
-    let api_one = warp::path!("one").and(warp::get()).and_then(handler_one);
+/// Our global unique user id counter.
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
 
-    let api_two = warp::path!("two").and(warp::get()).and_then(handler_two);
+/// Message variants.
+#[derive(Debug)]
+enum Message {
+    UserId(usize),
+    Reply(String),
+}
 
-    let api_routes = warp::path("api").and(api_one.or(api_two));
+#[derive(Debug)]
+struct NotUtf8;
+impl warp::reject::Reject for NotUtf8 {}
 
-    let routes = api_routes.or(warp::any().map(|| "Hello, World!"));
+/// Our state of currently connected users.
+///
+/// - Key is their id
+/// - Value is a sender of `Message`
+type Users = Arc<Mutex<HashMap<String, HashMap<usize, mpsc::UnboundedSender<Message>>>>>;
 
-    info!("running webserver");
+fn user_connected(
+    tenant_name: String,
+    users: Users,
+) -> impl Stream<Item = Result<Event, warp::Error>> + Send + 'static {
+    // Use a counter to assign a new unique ID for this user.
+    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
 
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .expect("runtime should start")
-        .block_on(async {
-            warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    eprintln!("new chat user: {}", my_id);
+
+    // Use an unbounded channel to handle buffering and flushing of messages
+    // to the event source...
+    let (tx, rx) = mpsc::unbounded_channel();
+    let rx = UnboundedReceiverStream::new(rx);
+
+    tx.send(Message::UserId(my_id))
+        // rx is right above, so this cannot fail
+        .unwrap();
+
+    // Save the sender in our list of connected users.
+    let mut tenants = users.lock().unwrap();
+    let tenant = tenants.get_mut(&tenant_name);
+    match tenant {
+        Some(tenant) => {
+            tenant.insert(my_id, tx);
+        }
+        None => {
+            let mut new_tenant = HashMap::new();
+            new_tenant.insert(my_id, tx);
+            tenants.insert(tenant_name, new_tenant);
+        }
+    }
+
+    // Convert messages into Server-Sent Events and return resulting stream.
+    rx.map(|msg| match msg {
+        Message::UserId(my_id) => Ok(Event::default().event("user").data(my_id.to_string())),
+        Message::Reply(reply) => Ok(Event::default().data(reply)),
+    })
+}
+
+fn user_message(tenant_name: String, my_id: usize, msg: String, users: &Users) {
+    let new_msg = format!("<User#{}>: {}", my_id, msg);
+
+    // New message from this user, send it to everyone else (except same uid)...
+    //
+    // We use `retain` instead of a for loop so that we can reap any user that
+    // appears to have disconnected.
+    let mut tenants = users.lock().unwrap();
+    let tenant = tenants.get_mut(&tenant_name);
+    match tenant {
+        Some(tenant) => {
+            tenant.retain(|uid, tx| {
+                if my_id == *uid {
+                    // don't send to same user, but do retain
+                    true
+                } else {
+                    // If not `is_ok`, the SSE stream is gone, and so don't retain
+                    tx.send(Message::Reply(new_msg.clone())).is_ok()
+                }
+            });
+        }
+        None => {
+            println!("なんか変だけど何もしない")
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    // Keep track of all connected users, key is usize, value
+    // is an event stream sender.
+    let users = Arc::new(Mutex::new(HashMap::new()));
+    // Turn our "state" into a new Filter...
+    let users = warp::any().map(move || users.clone());
+
+    // POST /tenant/:name/chat -> send message
+    let chat_send = warp::path!("tenant" / String / "chat" / usize)
+        .and(warp::post())
+        .and(warp::body::content_length_limit(500))
+        .and(warp::body::bytes().and_then(|body: Bytes| async move {
+            std::str::from_utf8(&body)
+                .map(String::from)
+                .map_err(|_e| warp::reject::custom(NotUtf8))
+        }))
+        .and(users.clone())
+        .map(|tenant_name, my_id, msg, users| {
+            user_message(tenant_name, my_id, msg, &users);
+            warp::reply()
         });
+
+    // GET /tenant/:name/chat -> messages stream
+    let chat_recv = warp::path!("tenant" / String / "chat")
+        .and(warp::get())
+        .and(users)
+        .map(|tenant_name, users| {
+            // reply using server-sent events
+            let stream = user_connected(tenant_name, users);
+            warp::sse::reply(warp::sse::keep_alive().stream(stream))
+        });
+
+    // GET /tenant/:name -> chat html
+    let tenant = warp::path!("tenant" / String).map(|_| {
+        warp::http::Response::builder()
+            .header("content-type", "text/html; charset=utf-8")
+            .body(CHAT_HTML)
+    });
+
+    // GET / -> index html
+    let index = warp::path::end().map(|| {
+        warp::http::Response::builder()
+            .header("content-type", "text/html; charset=utf-8")
+            .body(INDEX_HTML)
+    });
+
+    let routes = index.or(tenant).or(chat_recv).or(chat_send);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3000)).await;
 }
 
-async fn handler_one() -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(format!("You said one: {}", 1))
-}
+static INDEX_HTML: &str = r#"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Rust chat</title>
+    </head>
+    <body>
+        <h1>Rust chat</h1>
+        <script>
+        function onSubmit() {
+            setTimeout(() => {
+                location.href="/tenant/"+document.getElementById("name").value;
+            },10 );
+            return false;
+        }
+        </script>
+        <form onsubmit="onSubmit()">
+            <input id="name" type="text" placeholder="tenant name" />
+            <input type="submit">
+        </form>
+    </body>
+</html>
+"#;
 
-async fn handler_two() -> Result<impl warp::Reply, warp::Rejection> {
-    Ok(format!("You said two: {}", 2))
-}
+static CHAT_HTML: &str = r#"
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Warp Chat</title>
+    </head>
+    <body>
+        <h1>warp chat</h1>
+        <div id="chat">
+            <p><em>Connecting...</em></p>
+        </div>
+        <input type="text" id="text" />
+        <button type="button" id="send">Send</button>
+        <script type="text/javascript">
+        var uri = 'http://' + location.host + location.pathname + '/chat';
+        var sse = new EventSource(uri);
+        function message(data) {
+            var line = document.createElement('p');
+            line.innerText = data;
+            chat.appendChild(line);
+        }
+        sse.onopen = function() {
+            chat.innerHTML = "<p><em>Connected!</em></p>";
+        }
+        var user_id;
+        sse.addEventListener("user", function(msg) {
+            user_id = msg.data;
+        });
+        sse.onmessage = function(msg) {
+            message(msg.data);
+        };
+        send.onclick = function() {
+            var msg = text.value;
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", uri + '/' + user_id, true);
+            xhr.send(msg);
+            text.value = '';
+            message('<You>: ' + msg);
+        };
+        </script>
+    </body>
+</html>
+"#;
