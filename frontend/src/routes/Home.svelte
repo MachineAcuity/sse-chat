a0324f8cf50f8d80717e5b7dda7902ffd5c8c9f7
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2021-04-22 06:46:59 -0400

    Style room name screen (home) using tailwind.

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -10,12 +10,11 @@ const handleJoin = () => {
 };
 </script>
 
-<div>
-    <div>
-        <span>Room</span>
-        <input bind:value={roomId} type="text" />
+<div class="flex h-screen justify-center items-center bg-gray-400">
+    <div class="text-center">
+        <form class="m-4 flex">
+            <input bind:value={roomId} class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Room name"/>
+            <button on:click={handleJoin} class="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r">Join</button>
+        </form>
     </div>
-
-    <button on:click={handleJoin}>Join !</button>
-    <br>
 </div>
