0542ba6a802619a4d8705957b9212401b79a4c08
Aleksandar Chalakov <a.chalakov@codefoundries.com>
2021-04-25 00:48:30 -0400

    Upgrade to latest rollup-plugin-svelte with accompanying changes to rollup.config

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -5,6 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import replace from '@rollup/plugin-replace';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -20,7 +21,7 @@ export default {
 		inlineSvg({
 			// Removes specified tags and its children. You can specify tags by setting removingTags query array.
 			// default: false
-			removeTags: false,
+			removeTags: true,
 
 			// warning: this won't work unless you specify removeTags: true
 			// default: ['title', 'desc', 'defs', 'style']
@@ -44,14 +45,25 @@ export default {
 		}),
 
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: (css) => {
-				css.write('bundle.css');
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			},
+
+			// Warnings are normally passed straight to Rollup. You can
+			// optionally handle them here, for example to squelch
+			// warnings with a particular code
+			onwarn: (warning, handler) => {
+				// e.g. don't warn on <marquee> elements, cos they're cool
+				if (warning.code === 'a11y-distracting-elements') return;
+
+				// let Rollup handle all other warnings normally
+				handler(warning);
 			}
 		}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
